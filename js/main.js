2034c2d15ef248e5a76260468162ea1357a7e81e
lissalad <elissalaymon@gmail.com>
2022-09-26 15:45:31 -0700

    coins

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ PlayState.init = function () {
   }, this);
 };
 
-// load assets
+// preload assets
 PlayState.preload = function () {
   this.game.load.json("level:1", "data/level01.json");
 
@@ -62,7 +62,11 @@ PlayState.preload = function () {
 
   this.game.load.image("hero", "images/hero_stopped.png");
 
+  this.game.load.spritesheet("coin", "images/coin_animated.png", 22, 22);
+
   this.game.load.audio("sfx:jump", "audio/jump.wav");
+  this.game.load.audio("sfx:coin", "audio/coin.wav");
+
 };
 
 // create entities and set up game
@@ -71,6 +75,8 @@ PlayState.create = function () {
   this._loadLevel(this.game.cache.getJSON("level:1"));
   this.sfx = {
     jump: this.game.add.audio("sfx:jump"),
+    coin: this.game.add.audio("sfx:coin")
+
   };
 };
 
@@ -83,7 +89,16 @@ PlayState.update = function () {
 // sprite collisions
 PlayState._handleCollisions = function () {
   this.game.physics.arcade.collide(this.hero, this.platforms);
+
+  this.game.physics.arcade.overlap(this.hero, this.coins, this._onHeroVsCoin,
+    null, this);
 };
+
+// handle hero coin overlap
+PlayState._onHeroVsCoin = function (hero, coin) {
+  this.sfx.coin.play();
+  coin.kill();
+}
 
 // handle key controls
 PlayState._handleInput = function () {
@@ -99,13 +114,17 @@ PlayState._handleInput = function () {
 // --------- LEVEL SETUP ------------------- //
 PlayState._loadLevel = function (data) {
   this.platforms = this.game.add.group();
+  this.coins = this.game.add.group();
 
   data.platforms.forEach(this._spawnPlatform, this);
-  this._spawnCharacters({ hero: data.hero });
+  this._spawnCharacters({ hero: data.hero, spiders: data.spiders });
+  data.coins.forEach(this._spawnCoin, this);
 
   const gravity = 1200;
   this.game.physics.arcade.gravity.y = gravity;
 };
+
+// --------- CREATE SPRITES ------------------- //
 
 // create platforms
 PlayState._spawnPlatform = function (platform) {
@@ -120,6 +139,18 @@ PlayState._spawnPlatform = function (platform) {
 PlayState._spawnCharacters = function (data) {
   this.hero = new Hero(this.game, data.hero.x, data.hero.y);
   this.game.add.existing(this.hero);
+};
+
+// create coins
+PlayState._spawnCoin = function (coin) {
+  let sprite = this.coins.create(coin.x, coin.y, "coin");
+  sprite.anchor.set(0.5, 0, 5);
+
+  sprite.animations.add("rotate", [0, 1, 2, 1], 6, true);
+  sprite.animations.play("rotate");
+
+  this.game.physics.enable(sprite);
+  sprite.body.allowGravity = false;
 };
 
 // ----------- RUNS ------------------- //
